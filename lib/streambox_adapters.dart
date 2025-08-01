49d2ee0951b9b911d89a6572a83a2ae62b8fb492
Eugene <13645580+kalaganov@users.noreply.github.com>
2025-08-01 10:06:43 +0300

    chore: cleanup

--- lib/streambox_adapters.dart
+++
@@ -1,8 +0,0 @@
-
-import 'streambox_adapters_platform_interface.dart';
-
-class StreamboxAdapters {
-  Future<String?> getPlatformVersion() {
-    return StreamboxAdaptersPlatform.instance.getPlatformVersion();
-  }
-}
